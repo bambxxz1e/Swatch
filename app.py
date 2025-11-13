39938f0cabb0823129c873a447f9764d13cb87f9
윤성연 <s2427@e-mirim.hs.kr>
2025-11-14 02:24:37 +0900

    컬러팔레트 구현

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 from DBManager import DBManager
+from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +10,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 db = DBManager()
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 @app.route('/')
 def index():
@@ -37,12 +42,40 @@ def login():
         password = request.form['password']
 
         if db.login(user_id, password):
-            flash("로그인 성공!")
             return render_template('main.html')
         else:
             flash("아이디 또는 비밀번호가 틀렸습니다.")
             return redirect(url_for('login'))
     return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create_post():
+    uploaded_image = None
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        music_query = request.form.get('music_query')
+
+        file = request.files.get('image')
+        if file and file.filename != '':
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            if not os.path.exists(UPLOAD_FOLDER):
+                os.makedirs(UPLOAD_FOLDER)
+            file.save(filepath)
+            uploaded_image = filepath
+
+        # DB 저장 로직
+        # db.save_post(title=title, music_query=music_query, image_path=uploaded_image)
+
+        flash('게시물이 성공적으로 등록되었습니다!')
+        return redirect(url_for('create_post'))
+
+    return render_template('create_post.html', uploaded_image=uploaded_image)
 
 if __name__ == "__main__":
     app.run(debug=True)
